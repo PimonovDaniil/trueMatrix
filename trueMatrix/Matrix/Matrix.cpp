a3807bfa1e6732c919749a8b1cbfef9d3248f375
PimonovDaniil <PimonovDaniil@users.noreply.github.com>
2020-10-12 04:38:01 +0700

    перестройка
    Идёт по плану

--- trueMatrix/Matrix/Matrix.cpp
+++ trueMatrix/Matrix/Matrix.cpp
@@ -11,7 +11,8 @@ namespace mathTools
     int Matrix::num = 0; //начальное кол-во объектов класса
 
     /*инициализация (создание дубликата матрицы)*/
-    void Matrix::init(int rows, int cols, double** matrix) {
+    void Matrix::init(int rows, int cols, double* matrix) {
+        
         this->numObj = ++this->num;
 
         if (rows * cols == 0) {
@@ -22,37 +23,34 @@ namespace mathTools
 
         this->rows = rows; this->cols = cols;
 
-        double**& mtx = this->matrix = new double* [rows];
-        for (int i = 0; i < rows; i++) mtx[i] = new double[cols]();
+        this->matrix = new double[rows*cols];
 
         if (matrix == nullptr) return;
 
-        for (int i = 0; i < rows; i++)
-            for (int j = 0; j < cols; j++)
-                mtx[i][j] = matrix[i][j];
+        for (int i = 0; i < this->rows* this->cols; i++)
+            this->matrix[i] = matrix[i];
     }
 
     /*копирование объекта матрица*/
-    void Matrix::copy(const Matrix& other){
+    /*void Matrix::copy(const Matrix& other){
 
         if (this->matrix != nullptr) {
-            for (int i = 0; i < this->rows; i++)
-                delete[] this->matrix[i];
             delete[] this->matrix;
         }
 
         this->cols = other.cols;
         this->rows = other.rows;
 
-        this->matrix = new double* [this->rows];
-        for (int i = 0; i < this->rows; i++) {
-            this->matrix[i] = new double[this->cols];
-        } 
+        this->matrix = new double [this->rows * this->cols];
 
+        int k = 0;
         for (int i = 0; i < this->rows; i++)
-            for (int j = 0; j < this->cols; j++)
-                this->matrix[i][j] = other[i][j];
-    }
+            for (int j = 0; j < this->cols; j++) {
+                this->matrix[k] = other[i][j];
+                k++;
+            }
+                
+    }*/
 
     /*конструктор  без параметров*/
     Matrix::Matrix() {
@@ -61,8 +59,9 @@ namespace mathTools
     }
 
     /*принимает матрицу с размерами rows cols*/
-    Matrix::Matrix(int rows, int cols, double** matrix) {
+    Matrix::Matrix(int rows, int cols, double* matrix) {
         init(rows, cols, matrix);
+        
         if (debug) std::cout << "(матрица " << this->numObj << ", " << "конструктор 2)" << std::endl;
     }
 
@@ -73,7 +72,7 @@ namespace mathTools
     }
 
     /*принимает квадратную матрицу*/
-    Matrix::Matrix(int square, double** matrix) {
+    Matrix::Matrix(int square, double* matrix) {
         init(square, square, matrix);
         if (debug) std::cout << "(матрица " << this->numObj << ", " << "конструктор 4)" << std::endl;
     }
@@ -85,43 +84,41 @@ namespace mathTools
     }
 
     /*конструктор копирования*/
-    Matrix::Matrix(Matrix& other) {
+    /*Matrix::Matrix(Matrix& other) {
         this->matrix = nullptr;
         this->numObj = ++this->num;
         copy(other);
         if (debug) std::cout << "(матрица " << this->numObj << ", " << "конструктор копирования)" << std::endl;
-    }
+    }*/
 
     /*деструктор*/
     Matrix::~Matrix() {
         /*выкидываем за собой мусор*/
-        for (int i = 0; i < this->rows; i++)
-            delete[] this->matrix[i];
         delete[] this->matrix;
         if (debug) std::cout << "(матрица " << this->numObj << ", " << "деструтор)" << std::endl;
     }
 
     /*подмена понятий (перегрузка)*/
-    Matrix& Matrix::operator=(const Matrix& other) 
-    {
-        // Проверка на самоприсваивание
-        if (this != &other) copy(other);
-        //delete &other;
-        return *this;
-    }
+    //Matrix& Matrix::operator=(const Matrix& other) 
+    //{
+    //    // Проверка на самоприсваивание
+    //    if (this != &other) copy(other);
+    //    //delete &other;
+    //    return *this;
+    //}
 
     /*подмена понятий (перегрузка)*/
-    double*& Matrix::operator[](const int index){
-       return this->matrix[index];//TODO сделать проверку индексов
+    Matrix::MatrixRow Matrix::operator[](const int index){
+        return MatrixRow(this, index);//TODO сделать проверку индексов
     }
 
-    const double* Matrix::operator[](const int index) const
-    {
-        return this->matrix[index];//TODO сделать проверку индексов
-    }
+    //const double* Matrix::operator[](const int index) const
+    //{
+    //    return this->matrix[index];//TODO сделать проверку индексов
+    //}
 
     /*подмена понятий (перегрузка)*/
-    Matrix& Matrix::operator+=(const Matrix& other) {
+   /* Matrix& Matrix::operator+=(const Matrix& other) {
         if (this->isSum(other) == false)
             throw "Вычитание невозможно"; 
         for (int i = 0; i < this->rows; i++)
@@ -129,17 +126,17 @@ namespace mathTools
                 this->matrix[i][j] = this->matrix[i][j] + other[i][j];
 
         return *this;
-    }
+    }*/
 
     /*подмена понятий (перегрузка)*/
-    Matrix& Matrix::operator*=(double k) {
+    /*Matrix& Matrix::operator*=(double k) {
         for (int i = 0; i < this->rows; i++)
             for (int j = 0; j < this->cols; j++)
                 this->matrix[i][j] *= k;
         return *this;
-    }
+    }*/
 
-    Matrix& Matrix::operator*=(const Matrix& other) {
+    /*Matrix& Matrix::operator*=(const Matrix& other) {
         if (this->isMultiply(other) == false) 
             throw "Вычитание невозможно";
         double** newMatrix = new double* [this->rows];
@@ -160,10 +157,10 @@ namespace mathTools
         matrix = newMatrix;
 
         return *this;
-    }
+    }*/
 
     /*подмена понятий (перегрузка)*/
-    Matrix& Matrix::operator-=(const Matrix& other) {
+    /*Matrix& Matrix::operator-=(const Matrix& other) {
         if (this->isSum(other) == false) 
             throw "Вычитание невозможно";
      
@@ -172,35 +169,35 @@ namespace mathTools
                 this->matrix[i][j] -= other[i][j];
         
         return *this;
-    }
+    }*/
 
     /*подмена понятий (перегрузка)*/
-    Matrix Matrix::operator+(const Matrix& other) {
+    /*Matrix Matrix::operator+(const Matrix& other) {
         Matrix res(*this);
         res += other;
         return res;
-    }
+    }*/
 
     /*подмена понятий (перегрузка)*/
-    Matrix Matrix::operator-(const Matrix& other) {
+   /* Matrix Matrix::operator-(const Matrix& other) {
         Matrix res(*this);
         res -= other;
         return res;
-    }
+    }*/
 
     /*подмена понятий (перегрузка)*/
-    Matrix Matrix::operator*(const Matrix& other) {
+    /*Matrix Matrix::operator*(const Matrix& other) {
         Matrix res(*this);
         res *= other;
         return res;
-    }
+    }*/
 
     /*подмена понятий (перегрузка)*/
-    Matrix Matrix::operator*(double k) {
+    /*Matrix Matrix::operator*(double k) {
         Matrix res(*this);
         res *= k;
         return res;
-    }    
+    }  */  
 
     int Matrix::getCols() const{//узнать кол-во столбцов
         return this->cols;
@@ -214,63 +211,62 @@ namespace mathTools
     и в сторону уменьшения вплоть до удаления при пареметрах (0, 0).
     При уменьшении размеров матрицы лишние элементы удаляются, при увиличерии
     заполняются нулями. */
-    void Matrix::setLength(int rows, int cols) {
-        if ((rows == 0) || (cols == 0)) {
-            /*выкидываем за собой мусор*/
-            for (int i = 0; i < this->rows; i++)
-                delete[] this->matrix[i];
-            delete[] this->matrix;
+    //void Matrix::setLength(int rows, int cols) {
+    //    if ((rows == 0) || (cols == 0)) {
+    //        /*выкидываем за собой мусор*/
+    //        for (int i = 0; i < this->rows; i++)
+    //            delete[] this->matrix[i];
+    //        delete[] this->matrix;
 
-            this->matrix = nullptr;
-            this->rows = rows;
-            this->cols = cols;
-        }
-        else if ((rows < 0) || (cols < 0)) {
-            throw "Размер матрицы не может быть отрицательным";
-        }
-        else if (!((this->rows == rows) && (this->cols == cols))) {
-            /*инициализируем новую матрицу*/
-            double** newMatrix = new double* [rows];
-            for (int i = 0; i < rows; i++) {
-                newMatrix[i] = new double[cols];
-            }
+    //        this->matrix = nullptr;
+    //        this->rows = rows;
+    //        this->cols = cols;
+    //    }
+    //    else if ((rows < 0) || (cols < 0)) {
+    //        throw "Размер матрицы не может быть отрицательным";
+    //    }
+    //    else if (!((this->rows == rows) && (this->cols == cols))) {
+    //        /*инициализируем новую матрицу*/
+    //        double** newMatrix = new double* [rows];
+    //        for (int i = 0; i < rows; i++) {
+    //            newMatrix[i] = new double[cols];
+    //        }
 
-            /*переносим данные*/
-            for (int i = 0; i < rows; i++)
-                for (int j = 0; j < cols; j++)
-                    if ((i < this->rows) && (j < this->cols)) {
-                        newMatrix[i][j] = this->matrix[i][j];
-                    }
-                    else {
-                        newMatrix[i][j] = 0;
-                    }
+    //        /*переносим данные*/
+    //        for (int i = 0; i < rows; i++)
+    //            for (int j = 0; j < cols; j++)
+    //                if ((i < this->rows) && (j < this->cols)) {
+    //                    newMatrix[i][j] = this->matrix[i][j];
+    //                }
+    //                else {
+    //                    newMatrix[i][j] = 0;
+    //                }
 
-            /*выкидываем за собой мусор*/
-            for (int i = 0; i < this->rows; i++)
-                delete[] this->matrix[i];
-            delete[] this->matrix;
+    //        /*выкидываем за собой мусор*/
+    //        for (int i = 0; i < this->rows; i++)
+    //            delete[] this->matrix[i];
+    //        delete[] this->matrix;
 
-            this->matrix = newMatrix;
-            this->cols = cols;
-            this->rows = rows;
-        }
-    }
+    //        this->matrix = newMatrix;
+    //        this->cols = cols;
+    //        this->rows = rows;
+    //    }
+    //}
 
-    bool Matrix::isMultiply(const Matrix& other) {
+    /*bool Matrix::isMultiply(const Matrix& other) {
         return (this->matrix != nullptr && other.matrix != nullptr && this->cols == other.rows) ? true : false;
     }
 
     bool Matrix::isSum(const Matrix& other) {
         return ((this->cols == other.getCols()) && (this->rows == other.getRows()) && (other.matrix != nullptr)) ? true : false;
-    }
+    }*/
 
     double Matrix::getMax() {
         if ((this->rows > 0) && (this->cols > 0)) {
-            double max = matrix[0][0];
-            for (int i = 0; i < this->rows; i++)
-                for (int j = 0; j < this->cols; j++)
-                    if (matrix[i][j] > max)
-                        max = matrix[i][j];
+            double max = matrix[0];
+            for (int i = 0; i < this->rows * this->cols; i++)
+                    if (matrix[i] > max)
+                        max = matrix[i];
             return max;
         }
         else {
@@ -284,11 +280,11 @@ namespace mathTools
 
     double Matrix::getMin() {
         if ((this->rows > 0) && (this->cols > 0)) {
-            double min = matrix[0][0];
-            for (int i = 0; i < this->rows; i++)
+            double min = matrix[0];
+            for (int i = 0; i < this->rows * this->cols; i++)
                 for (int j = 0; j < this->cols; j++)
-                    if (matrix[i][j] < min)
-                        min = matrix[i][j];
+                    if (matrix[i] < min)
+                        min = matrix[i];
             return min;
         }
         else {
@@ -296,16 +292,16 @@ namespace mathTools
         }
     }
 
-    std::ostream& operator<< (std::ostream& os, const Matrix& matrix) {
-        //return os << "__" << std::endl;
-        for (int i = 0; i < matrix.getRows(); i++) {
-            for (int j = 0; j < matrix.getCols(); j++) {
-                os << std::setw(matrix.setting->getSetw()) << std::setprecision(matrix.setting->getPrecision()) << matrix[i][j] << " ";
-            }
-            os << std::endl;
-        }
-        return os;
-    }
+    //std::ostream& operator<< (std::ostream& os, const Matrix& matrix) {
+    //    //return os << "__" << std::endl;
+    //    for (int i = 0; i < matrix.getRows(); i++) {
+    //        for (int j = 0; j < matrix.getCols(); j++) {
+    //            os << std::setw(matrix.setting->getSetw()) << std::setprecision(matrix.setting->getPrecision()) << matrix[i][j] << " ";
+    //        }
+    //        os << std::endl;
+    //    }
+    //    return os;
+    //}
 
     settings::settings() {
         this->setw = 8;
@@ -337,6 +333,21 @@ namespace mathTools
     int settings::getPrecision()
     {
         return this->precision;
+    }
+    Matrix::MatrixRow::MatrixRow(const Matrix* owner, unsigned r)
+    {
+        this->m_owner = owner;
+        this->m_r = r;
+    }
+   
+    double Matrix::MatrixRow::operator[](unsigned r) const
+    {
+        return this->m_owner->matrix[this->m_r * this->m_owner->cols + r];
+    }
+
+    double& Matrix::MatrixRow::operator[](unsigned r)
+    {
+        return this->m_owner->matrix[this->m_r * this->m_owner->cols + r];
     }
 }
 
--- trueMatrix/Matrix/Matrix.h
+++ trueMatrix/Matrix/Matrix.h
@@ -7,40 +7,51 @@ namespace mathTools
 
 	class Matrix
 	{
-        friend std::ostream& operator<< (std::ostream& os, const Matrix& matrix);
+        //friend std::ostream& operator<< (std::ostream& os, const Matrix& matrix);
     private:
         int rows;
         int cols;
         int numObj;
-        double** matrix;
+        double* matrix;
         static int num;
         
-        void init(int rows, int cols,    double** matrix);/*инициализация (создание дубликата матрицы)*/
-        void copy(const Matrix& other);/*копирование объекта матрица*/
+        void init(int rows, int cols,    double* matrix);/*инициализация (создание дубликата матрицы)*/
+        //void copy(const Matrix& other);/*копирование объекта матрица*/
+
+        class MatrixRow {
+        private:
+            const Matrix* m_owner;
+            unsigned m_r;
+        public:
+            MatrixRow(const Matrix* owner, unsigned r);
+     
+            double operator[] (unsigned r) const;
+            double& operator[] (unsigned r);
+        };
 
     public:
         static settings* setting;
         static bool debug;
         Matrix(); /*конструктор  без параметров*/
-        Matrix(int rows, int cols, double** matrix); /*принимает матрицу с размерами rows cols*/
+        Matrix(int rows, int cols, double* matrix); /*принимает матрицу с размерами rows cols*/
         Matrix(int rows, int cols);/*матрица размеров rows cols заполненная нулями*/
-        Matrix(int square, double** matrix);/*принимает квадратную матрицу*/
+        Matrix(int square, double* matrix);/*принимает квадратную матрицу*/
         Matrix(int square);/*матрица размера square^2 заполненная нулями*/
-        Matrix(Matrix& other) ;/*конструктор копирования*/
+        //Matrix(Matrix& other) ;/*конструктор копирования*/
         ~Matrix();/*деструктор*/
 
         /*подмена понятий (перегрузка)*/
-        Matrix& operator=(const Matrix& other);
-        double*& operator[](const int index);
-        const double* operator[](const int index) const;
-        Matrix& operator+=(const Matrix& other);
+        //Matrix& operator=(const Matrix& other);
+        MatrixRow operator[](const int index);
+        //const double* operator[](const int index) const;
+        /*Matrix& operator+=(const Matrix& other);
         Matrix& operator-=(const Matrix& other);
         Matrix& operator*=(double k);
         Matrix& operator*=(const Matrix& other);
         Matrix operator+(const Matrix& other);
         Matrix operator-(const Matrix& other);
         Matrix operator*(const Matrix& other);
-        Matrix operator*(double k);
+        Matrix operator*(double k);*/
      
         
         static settings* settings();
@@ -51,10 +62,10 @@ namespace mathTools
         и в сторону уменьшения вплоть до удаления при пареметрах (0, 0).
         При уменьшении размеров матрицы лишние элементы удаляются, при увиличерии
         заполняются нулями. */
-        void setLength(int rows, int cols);
+        //void setLength(int rows, int cols);
 
-        bool isMultiply(const Matrix& other); //проверка на возможность умножения
-        bool isSum(const Matrix& other); //проверка на возможность сложения/вычитания
+        //bool isMultiply(const Matrix& other); //проверка на возможность умножения
+        //bool isSum(const Matrix& other); //проверка на возможность сложения/вычитания
 
         double getMax(); //вернуть максимальный элемент
         double getMin(); //вернуть минимальный элемент
